3615780ccb1671c93245a9018d3594f82b0983ce
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-04-30 15:46:44 +0530

    Separated Files

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,7 @@
+import 'package:first_app/components/or_divider.dart';
+import 'package:first_app/components/social_icon.dart';
 import 'package:first_app/screens/signup.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class LoginScreen extends StatefulWidget {
   static const ROUTE_LOGIN = "/login";
@@ -207,77 +208,6 @@ class _LoginScreenState extends State<LoginScreen> {
           press: () {},
         ),
       ],
-    );
-  }
-}
-
-class OrDivider extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: size.height * 0.02),
-      width: size.width * 0.8,
-      child: Row(
-        children: <Widget>[
-          buildDivider(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Text(
-              "OR",
-              style: TextStyle(
-                color: Color(0xFF6F35A5),
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          buildDivider(),
-        ],
-      ),
-    );
-  }
-
-  Expanded buildDivider() {
-    return Expanded(
-      child: Divider(
-        color: Color(0xFFD9D9D9),
-        height: 1.5,
-      ),
-    );
-  }
-}
-
-class SocialIcon extends StatelessWidget {
-  final String iconSrc;
-  final Function press;
-
-  const SocialIcon({
-    Key key,
-    this.iconSrc,
-    this.press,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 2,
-            color: Color(0xFFF1E6FF),
-          ),
-          shape: BoxShape.circle,
-        ),
-        child: SvgPicture.asset(
-          iconSrc,
-          height: 20,
-          width: 20,
-          color: Colors.deepPurple,
-        ),
-      ),
     );
   }
 }
